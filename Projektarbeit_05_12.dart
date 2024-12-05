30fc3f0ffb704c8746d8b383ae1aa4a7155b6154
MarcoKI040 <marco.appakademie@outlook.com>
2024-12-05 16:11:06 +0100

    Mit wirkürlichem inhalt gefüllt

--- Projektarbeit_05_12.dart
+++ Projektarbeit_05_12.dart
@@ -1,3 +1,13 @@
-void main () {
-  
+void main() {
+ String ampelFarbe = "Rot";
+ switch (ampelFarbe) {
+   case "Rot":
+     print("Stop! Bitte anhalten.");
+   case "Gelb":
+     print("Achtung! Bereit machen zum Anfahren.");
+   case "Grün":
+     print("Fahren! Die Straße ist frei.");
+   default:
+     print("Ungültige Ampelfarbe.");
+ }
 }
